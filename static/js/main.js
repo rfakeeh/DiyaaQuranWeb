ea37582d44c20d9ea217ed62694f51ab525a0918
rfakeeh <ranafakeeh87@gmail.com>
2024-01-27 18:41:33 +0300

    update all all

--- static/js/main.js
+++ static/js/main.js
@@ -25,46 +25,69 @@ $(document).ready(function () {
         let prompt_html = "<div class='row user-prompt'><div class='col-1'><img src='../static/img/user.png' class='user'></div><div class='col prompt'><p class='text'>"+prompt_text+"</p></div></div>";
         let service_url = "/process_text";
         let prompt_data = {text: prompt_text};
-        if (isUrl(prompt_text)) {
-            service_url = "/process_video";
-            service_url = "/process_text";
-        } 
+
         if (image_src!=null) {
             service_url = "/process_image";
             prompt_html = "<div class='row user-prompt'><div class='col-1'><img src='../static/img/user.png' class='user'></div><div class='col prompt'><img src='"+image_src+"' width='400'/></div></div>";
-            prompt_data = new FormData($('#form')[0]);
-            console.log(prompt_data);
-        }
-        $('#chat-body').append(prompt_html);
-        console.log(service_url);
-
-        $.ajax({
-            data: prompt_data,
-            type: "POST",
-            url: service_url,
-            cache: false,
-            processData: false,
-            contentType: false,
-        }).done(function (data) {
-
-            if (data.response) {
-                console.log(data);
-                let response_text = data.response;
-                let response_html = "<div class='row diyaa-response'><div class='col-1'><img src='../static/img/logo.png' class='diyaa'></div><div class='col response'><p class='text' id='response'>"+response_text+"</p></div></div>";
-                $('#chat-body').append(response_html);
-
-            } else {
+            $.ajax({
+                data: new FormData($('#form')[0]),
+                type: "POST",
+                url: service_url,
+                cache: false,
+                processData: false,
+                contentType: false,
+            }).done(function (data) {
+    
+                if (data.response) {
+                    console.log(data);
+                    let response_text = data.response;
+                    let response_html = "<div class='row diyaa-response'><div class='col-1'><img src='../static/img/logo.png' class='diyaa'></div><div class='col response'><p class='text' id='response'>"+response_text+"</p></div></div>";
+                    $('#chat-body').append(response_html);
+    
+                } else {
+                    let response_text = "عذرا حصل خطأ غير متوقع يرجي إعادة الارسال ";
+                    let response_html = "<div class='row diyaa-response'><div class='col-1'><img src='../static/img/logo.png' class='diyaa'></div><div class='col response'><p class='text' id='response'>"+response_text+"</p></div></div>";
+                    $('#chat-body').append(response_html);
+    
+                }
+            }).fail(function(jqXHR, textStatus, errorThrown) {            
                 let response_text = "عذرا حصل خطأ غير متوقع يرجي إعادة الارسال ";
                 let response_html = "<div class='row diyaa-response'><div class='col-1'><img src='../static/img/logo.png' class='diyaa'></div><div class='col response'><p class='text' id='response'>"+response_text+"</p></div></div>";
                 $('#chat-body').append(response_html);
+            });
+    
+        } else {
 
-            }
-        }).fail(function(jqXHR, textStatus, errorThrown) {            
-            let response_text = "عذرا حصل خطأ غير متوقع يرجي إعادة الارسال ";
-            let response_html = "<div class='row diyaa-response'><div class='col-1'><img src='../static/img/logo.png' class='diyaa'></div><div class='col response'><p class='text' id='response'>"+response_text+"</p></div></div>";
-            $('#chat-body').append(response_html);
-        });
+            $.ajax({
+                data: {
+                    text: prompt_text,
+                },
+                type: "POST",
+                url: service_url,
+            }).done(function (data) {
+    
+                if (data.response) {
+                    console.log(data);
+                    let response_text = data.response;
+                    let response_html = "<div class='row diyaa-response'><div class='col-1'><img src='../static/img/logo.png' class='diyaa'></div><div class='col response'><p class='text' id='response'>"+response_text+"</p></div></div>";
+                    $('#chat-body').append(response_html);
+    
+                } else {
+                    let response_text = "عذرا حصل خطأ غير متوقع يرجي إعادة الارسال ";
+                    let response_html = "<div class='row diyaa-response'><div class='col-1'><img src='../static/img/logo.png' class='diyaa'></div><div class='col response'><p class='text' id='response'>"+response_text+"</p></div></div>";
+                    $('#chat-body').append(response_html);
+    
+                }
+            }).fail(function(jqXHR, textStatus, errorThrown) {            
+                let response_text = "عذرا حصل خطأ غير متوقع يرجي إعادة الارسال ";
+                let response_html = "<div class='row diyaa-response'><div class='col-1'><img src='../static/img/logo.png' class='diyaa'></div><div class='col response'><p class='text' id='response'>"+response_text+"</p></div></div>";
+                $('#chat-body').append(response_html);
+            });
 
+        }
+        
+        $('#chat-body').append(prompt_html);
+        console.log(service_url);
         image_src = null;
         $('#text').val("");
 
